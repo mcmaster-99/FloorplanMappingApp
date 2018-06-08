02116e13553ea14ecbdd60c8dccaa872667b4eab
mcmaster-99 <bsulaimanh@gmail.com>
2018-06-08 10:28:24 -0500

    Updated invokeURL

--- js/config.js
+++ js/config.js
@@ -1,10 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_EmZYHJNib', 
-        userPoolClientId: '3mm8segt7vt7makq8tedia5bgj',
+        userPoolClientId: '1de9dc1303n9pdm0h7ljlsadvu',
+        identityPoolId: 'us-west-2:426460a4-e0a6-488b-aaa0-3d7ccab6a91a',
         region: 'us-west-2' 
     },
     api: {
-        invokeUrl: 'https://sjykfoqmx1.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://zd9se4rknj.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
