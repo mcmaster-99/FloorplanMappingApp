9678a80d6c837047ecd1aa987d0a10d4f987cbab
mcmaster-99 <bsulaimanh@gmail.com>
2018-06-12 08:39:20 -0500

    Save button writes all shapes to database

--- js/testing.js
+++ js/testing.js
@@ -12,10 +12,10 @@ var dynamodb = new AWS.DynamoDB();
 //=============================================================
 SVG.on(document, 'DOMContentLoaded', function() {
     var drawing = new SVG('draw').size(500, 500)
-                                .panZoom({zoomMin: 0.5, zoomMax: 20, zoomFactor: 0.2})
+                                //.panZoom({zoomMin: 0.5, zoomMax: 20, zoomFactor: 0.2})
 
 
-    var shapes = [];
+    var floorPlan = [];
 
     // LIVING ROOM
     var livingRoom = drawing.rect(150, 100)
@@ -55,10 +55,10 @@ SVG.on(document, 'DOMContentLoaded', function() {
     livingRoomSet.add(livingRoom)
                 .add(livingRoomDoor)
                 .add(livingRoomDevice)
-    shapes.push(livingRoomSet);*/
+    floorPlan.push(livingRoomSet);*/
 
-    // push shape to shapes array
-    shapes.push(livingRoom, livingRoomDoor, livingRoomDevice);
+    // push shape to floorPlan array
+    floorPlan.push(livingRoom, livingRoomDoor, livingRoomDevice);
 
 
     // BEDROOM
@@ -95,10 +95,10 @@ SVG.on(document, 'DOMContentLoaded', function() {
     bedRoomGroup.add(bedRoomDevice)
     bedRoomGroup.add(bedRoomDoor)*/
 
-    // push shape to shapes array
-    shapes.push(bedRoom, bedRoomDevice, bedRoomDoor);
+    // push shape to floorPlan array
+    floorPlan.push(bedRoom, bedRoomDevice, bedRoomDoor);
 
-    console.log(shapes);
+    console.log(floorPlan);
     // prints element data
     document.getElementById("print").onclick = function() {
         console.log(livingRoom.node.getBoundingClientRect().x);
@@ -107,96 +107,118 @@ SVG.on(document, 'DOMContentLoaded', function() {
         console.log(livingRoom.node.getBoundingClientRect().height);
     };
 
-    // UPDATE ROOM DATABASE
-    document.getElementById("save").onclick = function() {  
-
-        for (var i = 0; i < shapes.length; i++) {
-            shapes[i].draggable(false)
-
-            shapes[i].selectize(false).resize("stop")
-        }
-
-        var params = {
-            Item: {
-                "x": {
-                    S: String(livingRoom.node.getBoundingClientRect().x) //livingRoom.node.attributes[3].nodeValue
-                },
-                "y": {
-                    S: String(livingRoom.node.getBoundingClientRect().y)
-                },
-                "width": {
-                    S: String(livingRoom.node.getBoundingClientRect().width)
-                },
-                "height": {
-                    S: String(livingRoom.node.getBoundingClientRect().height)
-                },
-                "room_ID": {
-                    S: "livingRoom"
-                },
-                "floor": {
-                    S: livingRoom.node.attributes[3].nodeValue
-                }
-            },
-            ReturnConsumedCapacity: "TOTAL", 
-            TableName: "FloorPlan.test-at-test.com"
-        };
-
-        dynamodb.putItem(params, function(err, data) {
-            if (err) console.log(err, err.stack); // an error occurred 
-            else     console.log("Successfully saved and written to DB");           // successful response
-        
-        /*data = {
-            ConsumedCapacity: {
-             CapacityUnits: 1, 
-             TableName: "Music"
-            }
-        }*/
-        
-        });
-    }
 
 
     // ================ SIDE BAR TOOL SET FUNCTIONALITY ====================
+
+    // UPDATE ROOM DATABASE
+    document.getElementById("save").onclick = function() {  
+
+        // Stop dragging and resizing for all shapes
+        for (var i = 0; i < floorPlan.length; i++) {
+            floorPlan[i].draggable(false)
+            floorPlan[i].selectize(false).resize("stop")
+        }
+
+        /*
+        */
+        for (var i = 0; i < floorPlan.length; i++) {
+        
+            var params = {
+                Item: {
+                    "x": {
+                        S: String(floorPlan[i].node.getBoundingClientRect().x) //livingRoom.node.attributes[3].nodeValue
+                    },
+                    "y": {
+                        S: String(floorPlan[i].node.getBoundingClientRect().y)
+                    },
+                    "width": {
+                        S: String(floorPlan[i].node.getBoundingClientRect().width)
+                    },
+                    "height": {
+                        S: String(floorPlan[i].node.getBoundingClientRect().height)
+                    },
+                    "room_ID": {
+                        S: String(floorPlan[i])
+                    },
+                    "floor": {
+                        S: floorPlan[i].node.attributes[3].nodeValue
+                    }
+                },
+                ReturnConsumedCapacity: "TOTAL", 
+                TableName: "FloorPlan.test-at-test.com"
+            };
+
+            dynamodb.putItem(params, function(err, data) {
+                if (err) console.log(err, err.stack); // an error occurred 
+                else     console.log("Successfully saved and written to DB");           // successful response
+            
+            /*data = {
+                ConsumedCapacity: {
+                 CapacityUnits: 1, 
+                 TableName: "Music"
+                }
+            }*/
+            
+            });
+        }
+    }
 
     document.getElementById("drag-resize").onclick = function() {
         /*livingRoomGroup.selectize()
                     .resize({snapToAngle: 5})
                     .draggable({snapToGrid: 5})*/
-        for (var i = 0; i < shapes.length; i++) {
-            shapes[i].selectize()
+        for (var i = 0; i < floorPlan.length; i++) {
+            floorPlan[i].selectize()
                     .resize({snapToAngle: 5})
                     .draggable({snapToGrid: 5})
         }
 
     };
-    
-    document.getElementById("draw-rect").onclick = function() {
-        rect = drawing.rect()
-                            .attr({
-                                fill: 'white',
-                                stroke: '#CCCCCC',
-                                'stroke-width': 3
-                        })
-        rect.draw()
-        shapes.push(rect);
 
-        document.getElementById("resize").onclick = function() {
-            for (var i = 0; i < shapes.length; i++) {
-                //console.log("stop " + shapes[i] + " resize");
-                //console.log("start " + shapes[i] + " draggable");
-                shapes[i].selectize().resize()
-            }
-        };
+    
+    document.getElementById("print-fp-data").onclick = function() {
+
+        for (var i = 0; i < floorPlan.length; i++) {
+            console.log(floorPlan[i]);
+        }
     };
+    
 
     document.getElementById("clear").onclick = function() {
         //$("#draw").empty();
-        console.log(shapes);
-        for (var i = 0; i < shapes.length; i++) {
-            shapes.pop();
+        console.log(floorPlan);
+        for (var i = 0; i < floorPlan.length; i++) {
+            floorPlan.pop();
         }
-        console.log(shapes);
+        console.log(floorPlan);
     };
 
+
+    document.getElementById("draw-rect").onclick = function() {
+        
+        var rect = drawing.rect();
+
+        drawing.on('mousedown', function(e){
+            rect.draw(e)
+                .attr({
+                    fill: 'white',
+                    stroke: '#CCCCCC',
+                    'stroke-width': 3
+                })
+        }, false);
+
+        drawing.on('mouseup', function(e){
+            rect.draw('stop', e);
+            floorPlan.push(rect);
+        }, false);
+        
+        rect.on('drawstop', function(){
+            rect.draw('stop');
+        });
+        
+        console.log(floorPlan);
+        
+    };
 
 })
