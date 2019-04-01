3928f712ca9bdfddf0663f8dca4c732123713dbd
mcmaster-99 <bsulaimanh@gmail.com>
2019-04-01 09:26:40 -0500

    Got React Router to render. Deleted redundant file/page. Created 'Inlo Node Found' page.

--- js/inloNodeFound.js
+++ js/inloNodeFound.js
@@ -49,7 +49,7 @@ class AccessPointNotExist extends React.Component {
 
 			<div>
 				<NavBar/>
-				<Prompt/>
+				<Jumbotron/>
 			</div>
 
         );
@@ -60,73 +60,37 @@ class AccessPointNotExist extends React.Component {
 class NavBar extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			title: 'Describe Room'
-		};
 	}	
 
 	render() {
 		return (
 
-
 			<div id="navbar-div">
 		      <nav>
-		        <a href="#"><img src="images/theinlo.png" id="inlo-banner"></img></a>
-
-		        <div id="right-nav-icons-div">
-		            <div id="add-devices-dropdown-div">
-
-		                <button id="dropdown-btn">
-		                    <img src="images/username.png" id="onboard-device" className="right-nav-icons"></img>
-		                </button>
-		                <div id="dropdown-menu">
-		                    <li><a id="addID" href="#addID">Add Inlo Device</a></li>
-		                    <li><a id="addTPD" href="#addTPD">Add Third Party Device</a></li>
-		                </div>
-		                
-		            </div>
-
-		            <a href="#"><img src="images/email.png" className="right-nav-icons"></img></a>
-		            <a href="#"><img src="images/home.png" className="right-nav-icons"></img></a>
-		        </div>
-
+		        <img src="images/theinlo.png" id="inlo-banner"></img>
 		      </nav>
 		    </div>
-
 
         );
 	}
 }
 
-class AddAccessPointFAQPrompt extends React.Component {
+class Jumbotron extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			text: 'Inlo uses the technology in your smart phone to connect to the Inlo hub. You must use your Inlo Mobile App to add your hub.'
-
-	            +'Once you have added your hub, you may add additional inlo nodes and third party devices to your account.'
-
-	            +'You can identify which Inlo node is your hub by checking for a label on the back of your nodes.'
-		};
+			title: "Inlo Node Found!",
+			prompt: "Tell us where your Inlo node is."
 	}	
-
-	handleClick() {
-		window.location.href = "dashboard.html";
-	}
 
 	render() {
 		return (
 
-			<div id="how-to-text-div">
-		        <h3 id="how-to-title">FAQ</h3>
+			<div id="jumbotron-div">
+		        
+				<h1 id="title">{this.state.title}</h1>
 
-		        <p id="how-to-text">
-		            {this.state.text}
-		        </p>
-
-		        <div id="how-to-links-div">
-		            <Link to="/">Go Back Home</Link>
-		        </div>
+				<h3 id="prompt">{this.state.prompt}</h3>
 
 		    </div>
 
@@ -179,6 +143,7 @@ class Prompt extends React.Component {
 
 ReactDOM.render((
 	<Router>
+		<AccessPointNotExist/>
 		<Route path="/AccessPointNotExist" component={AccessPointNotExist}/>
 		<Route path="/AddAccessPointFAQPrompt/" component={AddAccessPointFAQPrompt}/>
 	</Router>
