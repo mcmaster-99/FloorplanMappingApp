7f0b2ab1a40f197b73143014283afc3ea30f171a
mcmaster-99 <bsulaimanh@gmail.com>
2019-05-28 13:03:04 -0500

    fixed click event on room instead of parent svg

--- js/placeNewNode.js
+++ js/placeNewNode.js
@@ -29,42 +29,52 @@ ReactDOM.render((
 	<NavBar/>
 ),document.getElementById("root"));*/
 
-let roomData;
 
 SVG.on(document, 'DOMContentLoaded', function () {
 
-	function render_node() {
-		console.log("here")
+	let draw = new SVG('draw').size("100%", "100%").attr({id: "svg"}),
+			encoded =  window.location.href,
+			room,
+			roomID,
+			roomData;
+
+	try {
+		// decoding URL to get params
+		let str = decodeURIComponent(encoded),
+			roomID = str.substr(str.indexOf("=")+1);
+
+		console.log(roomID)
+	} catch(e) { // catches a malformed URI
+	  console.error(e);
 	}
 
-	let draw = new SVG('draw').size("100%", "100%")
-	$("defs").detach()
-	let rect = draw.rect(100, 100)
-									.attr({ 
-										fill: 'none', 
-										stroke: "black",
-										id: "room1"
-									})
-
-	$('#draw').on('click', 'rect#room1', function(){
-	  alert("clicked")
-	}, false);
-
-	var encoded =  window.location.href,
-		roomID,
-		roomData;
-
-		try {
-			// decoding URL to get params
-			let str = decodeURIComponent(encoded),
-				roomID = str.substr(str.indexOf("=")+1);
-
-			console.log(roomID)
-		} catch(e) { // catches a malformed URI
-		  console.error(e);
-		}
-
+	const bind_click = function() {
 		
+		$("rect").click(function(e){	
+			console.log(e)
+			draw.image("images/inlo-device.png", 15, 10).attr({x:e.offsetX, y:e.offsetY});
+		})
+	}
+
+	const render_room = function() {
+		room = draw.rect(100, 100)
+								.attr({ 
+									x: 50,
+									y: 50,
+									fill: 'white', 
+									stroke: "black",		
+									id: "room1"
+								})
+		
+		console.log($("#draw"))
+	}
+
+	render_room()
+	bind_click()
+	
+
+
+	const fetch_room_data = function(render_room, bind_click) {
 		$.ajax({
 		  method: 'GET',
 		  url: String(_config.api.inloApiUrl) + '/v1/floorplan',
@@ -87,6 +97,9 @@ SVG.on(document, 'DOMContentLoaded', function () {
 				}
 			}
 		}
+	}
+
+	//fetch_room_data(render_room, bind_click);
 
   // if user has made any changes, ask before exiting current page
 
